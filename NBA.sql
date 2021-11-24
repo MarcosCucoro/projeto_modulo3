48703ec7322be417e05372256462ba45aa2a55b0
Marcos Cucoro <marcos.cucoro@hotmail.com>
2021-11-23 22:28:19 -0300

    Atualização do novo banco de dados e tema NBA

---
+++ NBA.sql
@@ -0,0 +1,107 @@
+CREATE DATABASE `NBA_STATICS`;
+USE `NBA_STATICS`;
+
+CREATE TABLE `TIMES` (
+  `TIME_ID` INT PRIMARY KEY,
+  `LIGA_ID` INT,
+  `ABREVIACAO` CHAR(3),
+  `NOME_TIME` VARCHAR(50),
+  `ANO_FUNDACAO` INT(4),
+  `CIDADE` VARCHAR(50),
+  `ARENA` VARCHAR(100),
+  `CAPACIDADE_ARENA` INT,
+  `PROPRIETARIO_TIME` VARCHAR(100),
+  `DIRETOR_GERAL` VARCHAR(100),
+  `TREINADOR` VARCHAR(100),
+  `CLUBE_AFILIADO` VARCHAR(100)
+);
+
+CREATE TABLE `CLASSIFICACAO` (
+  `TIME_ID` INT,
+  `LIGA_ID` INT,
+  `TEMPORADA` INT,
+  `DATA_POSICAO` DATE,
+  `CONFERENCIA` VARCHAR(10),
+  `JOGOS` INT,
+  `VITORIAS` INT,
+  `DERROTAS` INT,
+  `PCT_VITORIAS` DECIMAL(5,3),
+  `RECORDES_CASA` VARCHAR(6),
+  `RECORDES_FORA` VARCHAR(6)
+);
+
+CREATE TABLE `JOGOS` (
+  `DATA_JOGO` DATE,
+  `JOGO_ID` INT PRIMARY KEY,
+  `JOGO_STATUS` CHAR(5),
+  `TIME_CASA_ID` INT,
+  `TIME_VISITANTE_ID` INT,
+  `TEMPORADA` INT,
+  `PONTOS_CASA` INT,
+  `PCT_FG_CASA` DECIMAL(4,1),
+  `PCT_FT_CASA` DECIMAL(4,1),
+  `PCT_FG3_CASA` DECIMAL(4,1),
+  `AST_CASA` DECIMAL(4,1),
+  `REB_CASA` DECIMAL(4,1),
+  `PONTOS_VISITANTE` INT,
+  `PCT_FG_VISITANTE` DECIMAL(4,1),
+  `PCT_FT_VISITANTE` DECIMAL(4,1),
+  `PCT_FG3_VISITANTE` DECIMAL(4,1),
+  `AST_VISITANTE` DECIMAL(4,1),
+  `REB_VISITANTE` DECIMAL(4,1),
+  `TIME_CASA_VENCE` CHAR(1)
+);
+
+CREATE TABLE `DETALHE_JOGO` (
+  `JOGO_ID` INT,
+  `TIME_ID` INT,
+  `JOGADOR_ID` INT,
+  `MIN_QUADRA` TIME,
+  `FGM` DECIMAL(4,1),
+  `FGA` DECIMAL(4,1),
+  `PCT_FG` DECIMAL(4,1),
+  `FG3M` DECIMAL(4,1),
+  `FG3A` DECIMAL(4,1),
+  `PCT_FG3` DECIMAL(4,1),
+  `FTM` DECIMAL(4,1),
+  `FTA` DECIMAL(4,1),
+  `PCT_FT` DECIMAL(4,1),
+  `REB` DECIMAL(4,1),
+  `BLK` DECIMAL(4,1),
+  `PTS` DECIMAL(4,1)
+ );
+
+CREATE TABLE `JOGADOR` (
+  `NOME_JOGADOR` VARCHAR(100),
+  `TIME_ID` INT,
+  `JOGADOR_ID` INT PRIMARY KEY,
+  `TEMPORADA` INT
+);
+
+
+/* GLOSSÁRIO */
+/*
+PCT_FG - % Arremessos certos
+PCT_FT - % Aproveitamento nos lances livres
+PCT_FG3 - % de Arremessos de 3 pontos convertidos
+AST - Assistência por jogo
+MIN_QUADRA - Tempo do jogador em quadra em minutos
+FGM - Média de arremessos convertidos
+FGA - Média de Tentativas de arremessos
+FG3M - Média de Arremessos de 3 pontos convertidos
+FG3A - Média de Tentativas de arremessos de 3 pontos
+FTM - Média de Lances Livres convertidos
+FTA - Média de Tentativas de lances livres
+REB - Rebotes por jogo
+BLK - Tocos por jogo
+PTS - Pontos por jogo
+*/
+
+ 
+
+
+
+
+
+
+
