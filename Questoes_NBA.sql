56b8493a885c24112f89439fa861509757180d88
Marcos Cucoro <marcos.cucoro@hotmail.com>
2021-11-24 16:12:03 -0300

    Inclusão do arquivo com as perguntas feitas ao banco de dados e os Select com as respostas.

---
+++ Questoes_NBA.sql
@@ -0,0 +1,23 @@
+/* PERGUNTAS */
+/* Quais os melhores times de cada liga? */
+
+/* Quais os melhores times de basquete? */
+
+/* Qual o melhor time de cada temporada(ano)? */
+
+/* Quais times tem? */
+SELECT * FROM TIMES;
+
+/* Times mais antigos? */
+SELECT CIDADE, NOME_TIME, ANO_FUNDACAO FROM TIMES
+ORDER BY ANO_FUNDACAO;
+
+/* Melhores jogadores da temporada? */
+
+/* Quais jogadores fizeram mais sesta na temporada X? */
+
+/* Quem mais deu assistencia? */
+
+/* Maiores tocos da liga? */
+
+/* Evolução dos times */
